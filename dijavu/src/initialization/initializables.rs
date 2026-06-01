357c425620388b7b920bef91320c8b6316334297
jungnik <git@jungnik.org>
2026-06-01 22:02:40 +0200

    feat: reintroduce `DropValue`

--- dijavu/src/initialization/initializables.rs
+++ dijavu/src/initialization/initializables.rs
@@ -5,6 +5,7 @@ use crate::data::DataValue;
 use crate::{InitInjector, Initializable, Injectable, InjectorBuilder, NewInitValue};
 use futures::future::BoxFuture;
 use std::convert::Infallible;
+use std::marker::PhantomData;
 use std::ops::Deref;
 
 /// Injects an [`Injectable`].
@@ -96,6 +97,30 @@ impl<T> Deref for Value<T> {
 type InitializablesBuilder<T> = Box<
     dyn for<'a> FnOnce(&'a mut InjectorBuilder) -> BoxFuture<'a, crate::Result<T>> + Send + Sync,
 >;
+
+/// A value that is used only during initialization and is dropped on build.
+///
+/// `DropValue<T>` allows modifying a value of type `T` during initialization, which is dropped on
+/// build.
+/// It implements [`NewInitValue`] if `T` implements `Default` and uses `T`'s default value as the
+/// initialization value.
+pub struct DropValue<T>(PhantomData<T>);
+
+impl<T: DataValue> Initializable for DropValue<T> {
+    type Init = T;
+
+    async fn build(_: Self::Init, _: &mut InjectorBuilder) -> crate::Result<Self> {
+        Ok(Self(PhantomData))
+    }
+}
+
+impl<T: Default + DataValue> NewInitValue for DropValue<T> {
+    type Error = Infallible;
+
+    async fn new_init(_: &mut InitInjector) -> Result<Self::Init, Self::Error> {
+        Ok(T::default())
+    }
+}
 
 /// A collection of initializables, usually used for collecting objects implementing a trait.
 ///
